4192043c823944470b800f48c3fb0bff33b2ff33
lyw6357 <lyw6357@naver.com>
2023-04-30 10:07:43 -0400

    term project running check

--- host/main.c
+++ host/main.c
@@ -34,8 +34,13 @@
 
 /* To the the UUID (found the the TA's h-file(s)) */
 #include <TEEencrypt_ta.h>
+#include <unistd.h>
 
-int main(void)
+#define RSA_KEY_SIZE 1024
+#define RSA_MAX_PLAIN_LEN_1024 86 // (1024/8) - 42 (padding)
+#define RSA_CIPHER_LEN_1024 (RSA_KEY_SIZE / 8)
+
+int main(int argc, char *argv[])
 {
 	TEEC_Result res;
 	TEEC_Context ctx;
@@ -44,25 +49,129 @@ int main(void)
 	TEEC_UUID uuid = TA_TEEencrypt_UUID;
 	uint32_t err_origin;
 
-	/* Initialize a context connecting us to the TEE */
-	res = TEEC_InitializeContext(NULL, &ctx);
-
-	res = TEEC_OpenSession(&ctx, &sess, &uuid,
-			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
+	char plaintext[64] = {0, };
+	char ciphertext[64] = {0, };
+	char clear[RSA_MAX_PLAIN_LEN_1024];
+	char ciph[RSA_CIPHER_LEN_1024];
+	int encKey;
+	FILE* fp;
 	
-	memset(&op, 0, sizeof(op));
+	res = TEEC_InitializeContext(NULL, &ctx);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
 
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_NONE,
-					 TEEC_NONE, TEEC_NONE);
-	op.params[0].value.a = 42;
+	res = TEEC_OpenSession(&ctx, &sess, &uuid, TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x", res, err_origin);
 
-	// printf("Invoking TA to increment %d\n", op.params[0].value.a);
-	res = TEEC_InvokeCommand(&sess, TA_TEEencrypt_CMD_INC_VALUE, &op,
-				 &err_origin);
+   	memset(&op, 0, sizeof(op));
+   	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_OUTPUT, 
+					 TEEC_VALUE_INOUT, 
+					 TEEC_MEMREF_TEMP_INPUT, 
+					 TEEC_MEMREF_TEMP_OUTPUT); 
 
-	TEEC_CloseSession(&sess);
+	if (strcmp(argv[1], "-e") == 0){
+		fp = fopen(argv[2], "r");
+		if(fp == NULL){
+			perror("Plaintext file not found");
+			return 1;
+		}
+		fread(plaintext, 1, 64, fp);
+		fclose(fp);
 
-	TEEC_FinalizeContext(&ctx);
+		printf("\n==================== Encryption ====================\n");
+		printf("-------------------- Plaintext --------------------\n%s\n", plaintext);
 
-	return 0;
+		if (strcmp(argv[3], "Caesar") == 0){
+   			op.params[0].tmpref.buffer = plaintext;
+   			op.params[0].tmpref.size = 64;
+
+			res = TEEC_InvokeCommand(&sess, TA_TEEencrypt_CMD_ENCRYPT, &op, &err_origin);
+			if (res != TEEC_SUCCESS)
+				errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x", res, err_origin);
+			memcpy(ciphertext, op.params[0].tmpref.buffer, 64);
+			
+			printf("-------------------- Ciphertext --------------------\n%s\n", ciphertext);
+			
+                	fp = fopen("ciphertext.txt", "w");
+			fputs(ciphertext, fp); 
+			fclose(fp);
+
+			fp = fopen("encryptedkey.txt", "w");
+			int enc_key = op.params[1].value.a;
+			fprintf(fp, "%d", enc_key);
+			fclose(fp);
+		}
+		else if(strcmp(argv[3], "RSA") == 0){
+			op.params[2].tmpref.buffer = clear;
+			op.params[2].tmpref.size = RSA_MAX_PLAIN_LEN_1024;
+			op.params[3].tmpref.buffer = ciph;
+			op.params[3].tmpref.size = RSA_CIPHER_LEN_1024;
+		
+			res = TEEC_InvokeCommand(&sess, TA_RSA_CMD_GENKEYS, &op, &err_origin);
+			if (res != TEEC_SUCCESS)
+				errx(1, "\nTEEC_InvokeCommand failed %#x\n", res);
+			memcpy(op.params[2].tmpref.buffer, plaintext, RSA_MAX_PLAIN_LEN_1024);
+			
+			res = TEEC_InvokeCommand(&sess, TA_RSA_CMD_ENCRYPT, &op, &err_origin);
+			if(res != TEEC_SUCCESS)
+				errx(1, "\nTEEC_InvokeCommand failed 0x%x origin 0x%x\n", res, err_origin);
+			memcpy(ciph, op.params[3].tmpref.buffer, 64);
+			
+			printf("\n-------------------- Ciphertext --------------------\n%s\n", ciph);
+
+			fp = fopen("ciphertext_RSA.txt", "w");
+			fputs(ciph, fp);
+			fclose(fp);
+		}
+		else{
+			perror("Undefined algorithmn\n");
+			return 1;
+		}
+   	}
+   	else if (strcmp(argv[1], "-d") == 0){ 
+   		op.params[0].tmpref.buffer = ciphertext;
+   		op.params[0].tmpref.size = 64;
+
+		fp = fopen(argv[2], "r");
+		if (fp == NULL){
+			perror("Ciphertext file not found");
+			return 1;
+		}
+		fread(ciphertext, 1, 64, fp);
+		fclose(fp);
+
+		printf("\n==================== Decryption ====================\n");
+		printf("-------------------- Ciphertext --------------------\n%s\n", ciphertext);
+
+		fp = fopen(argv[3], "r");
+		if (fp == NULL){
+			perror("Encryptedkey file not found");
+			return 1;
+		}
+		fscanf(fp, "%d", &encKey);
+		fclose(fp);
+        
+		memcpy(op.params[0].tmpref.buffer, ciphertext, 64);
+		op.params[1].value.a = encKey;
+
+		res = TEEC_InvokeCommand(&sess, TA_TEEencrypt_CMD_DECRYPT, &op, &err_origin);
+		if (res != TEEC_SUCCESS)
+			errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x", res, err_origin);
+		memcpy(plaintext, op.params[0].tmpref.buffer, 64);
+		
+		printf("-------------------- Plaintext --------------------\n%s\n", plaintext);
+
+		fp = fopen("plaintext.txt", "w");
+		fputs(plaintext, fp);
+		fclose(fp);
+   	}
+   	else{
+		perror("Undefined command option\n");
+		return 1;
+   	}
+
+   	TEEC_CloseSession(&sess);
+   	TEEC_FinalizeContext(&ctx);
+   	return 0;
 }
